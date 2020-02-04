2d2c52b8393a0d34f7ae222e57187c01000af646
kangyuxin <1549069709@qq.com>
2020-02-04 11:46:27 +0800

    总体界面设计

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,67 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/dashboard'
+    },
+    {
+      path: '/',
+      component: () => import(/* webpackChunkName: "home" */ '../views/common/Home.vue'),
+      meta: { title: '自述文件' },
+      children: [
+        {
+          path: '/dashboard',
+          component: () => import(/* webpackChunkName: "dashboard" */ '../views/Dashboard'),
+          meta: { title: '系统首页' }
+        },
+        {
+          path: '/table',
+          component: () => import(/* webpackChunkName: "table" */ '../views/BaseTable'),
+          meta: { title: '基础表格' }
+        },
+        {
+          // 图片上传组件
+          path: '/upload',
+          component: () => import(/* webpackChunkName: "upload" */ '../views/Upload'),
+          meta: { title: '文件上传' }
+        },
+        {
+          // 国际化组件
+          path: '/i18n',
+          component: () => import(/* webpackChunkName: "i18n" */ '../views/I18n'),
+          meta: { title: '国际化' }
+        },
+        {
+          // 权限页面
+          path: '/permission',
+          component: () => import(/* webpackChunkName: "permission" */ '../views/Permission'),
+          meta: { title: '权限测试', permission: true }
+        },
+        {
+          path: '/404',
+          component: () => import(/* webpackChunkName: "404" */ '../views/error/404'),
+          meta: { title: '404' }
+        },
+        {
+          path: '/403',
+          component: () => import(/* webpackChunkName: "403" */ '../views/error/403'),
+          meta: { title: '403' }
+        }
+      ]
+    },
+    {
+      path: '/login',
+      component: () => import(/* webpackChunkName: "login" */ '../views/Login'),
+      meta: { title: '登录' }
+    },
+    {
+      path: '*',
+      redirect: '/404'
+    }
+  ]
 })
-
-export default router
